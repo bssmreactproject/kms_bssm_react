74b24c4c69cbfa5bd20ee3a07d2c5df7713c692c
kimminse3380 <mss2214@naver.com>
2022-07-05 23:23:23 +0900

    init

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Sidebar.css";
+import { Button } from '@material-ui/core';
+import AddIcon from "@material-ui/icons/Add";
+
+function Sidebar(){
+    return (
+        <div className='sidebar'>
+            <Button startIcon={<AddIcon fontSize="large" className='sidebar_compose'/>}>Compose</Button>
+        </div>
+    );
+}
+
+export default Sidebar;
